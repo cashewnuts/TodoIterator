27c9a57883ead7dfa6748242daa8268decbe9859
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-05-07 23:29:10 +0900

    Add loading icon for app bar.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import FolderSpecialIcon from '@material-ui/icons/FolderSpecial'
+import SyncIcon from '@material-ui/icons/Sync'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { createLogger } from './services/logger'
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
@@ -34,13 +36,17 @@ export default function App() {
   const classes = useStyles()
   const { storeService } = useContext(ServiceContext)
   const [driveAuthed, setDriveAuthed] = useState(false)
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     const initFn = async () => {
       try {
+        setLoading(true)
         await storeService.init()
         setDriveAuthed(storeService.isSignedIn)
       } catch (err) {
         logger.error('Error while App initiating', err)
+      } finally {
+        setLoading(false)
       }
     }
     initFn()
@@ -58,8 +64,20 @@ export default function App() {
           </Link>
           <div className={classes.grow} />
           <div className={classes.taskBar}>
+            {loading && (
+              <IconButton color="inherit" disabled disableRipple>
+                <CircularProgress color="secondary" size={30} />
+              </IconButton>
+            )}
             {driveAuthed ? (
-              <GdriveIcon fontSize="large" />
+              <>
+                <IconButton color="inherit">
+                  <SyncIcon />
+                </IconButton>
+                <IconButton color="inherit">
+                  <GdriveIcon fontSize="large" />
+                </IconButton>
+              </>
             ) : (
               <IconButton
                 aria-label="Auth with gdrive"
