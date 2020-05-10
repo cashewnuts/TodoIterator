7a69fbd2f46de4d5cdabdb3982afd4c52b99f4c7
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-05-10 20:45:43 +0900

    Update queue list display.

--- src/app/TodoQueueApp.tsx
+++ src/app/TodoQueueApp.tsx
@@ -1,13 +1,16 @@
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
 import { db } from '../services/local-db'
 import { createLogger } from '../services/logger'
+import { ITask } from '../models/task'
 const logger = createLogger({ filename: 'TodoQueueApp.tsx' })
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const TodoQueueApp = (props: PropsWithChildren<unknown>) => {
+  const [taskQueue, setTaskQueue] = useState<ITask[]>([])
   useEffect(() => {
     const asyncFn = async () => {
       const leafTasks = await db.tasks.where({ nodeType: 'leaf' }).toArray()
+      setTaskQueue(leafTasks)
       logger.debug('leafTasks', leafTasks)
     }
     asyncFn()
@@ -15,6 +18,10 @@ const TodoQueueApp = (props: PropsWithChildren<unknown>) => {
   return (
     <>
       <h1>hello</h1>
+      <ul>
+        {taskQueue &&
+          taskQueue.map((task) => <li key={task.id}>{task.name}</li>)}
+      </ul>
     </>
   )
 }
